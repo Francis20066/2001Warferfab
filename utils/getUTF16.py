3ba8ac28a7c6540811e38f6cb4b639e835d585e3
Francis20066 <24084102@st.cidp.edu.cn>
2026-06-04 10:22:20 +0800

    更新.gitignore以忽略utils目录下的文本文件；修改getUTF16.py以优化输出格式

--- utils/getUTF16.py
+++ utils/getUTF16.py
@@ -9,9 +9,11 @@ with open("utils/input.txt","r",encoding="utf-8") as f:
 parses = text.split('\n')
     
 for s in parses:
-    o = ','.join(f"0{ord(ch):04X}h" for ch in s)+",0000h"
-    result.append(o)
-
+    if s != "":
+        o = ','.join(f"0{ord(ch):04X}h" for ch in s)+",0000h"
+        result.append(s+':\t'+o)
+    else:
+        result.append("")
 output = '\n'.join(result)
 
 with open("utils/output.txt",mode='w',encoding="utf-8") as f:
